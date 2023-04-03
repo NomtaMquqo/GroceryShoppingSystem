aa07cce20bb320e8427e12cc468ea7df09ecf415
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2023-04-03 15:01:42 +0200

    Basecode is created. Store class is added.

---
+++ src/main/java/domain/Store.java
@@ -0,0 +1,109 @@
+/**
+ * Store.java
+ * This is a POJO class for store entity
+ * @author Asive Sibeko 219475296
+ * 03 April 2023
+ */
+
+package domain;
+
+public class Store {
+
+    private int storeId;
+    private String storeName;
+    private String storeAddress;
+    private String storeDescription;
+
+    private Store(){
+
+    }
+
+    private Store(Builder builder) {
+        this.storeId = builder.storeId;
+        this.storeName = builder.storeName;
+        this.storeAddress = builder.storeAddress;
+        this.storeDescription = builder.storeDescription;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStoreAddress() {
+        return storeAddress;
+    }
+
+    public void setStoreAddress(String storeAddress) {
+        this.storeAddress = storeAddress;
+    }
+
+    public String getStoreDescription() {
+        return storeDescription;
+    }
+
+    public void setStoreDescription(String storeDescription) {
+        this.storeDescription = storeDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "storeId=" + storeId +
+                ", storeName='" + storeName + '\'' +
+                ", storeAddress='" + storeAddress + '\'' +
+                ", storeDescription='" + storeDescription + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+        private int storeId;
+        private String storeName;
+        private String storeAddress;
+        private String storeDescription;
+
+        public Builder setStoreId(int storeId) {
+            this.storeId = storeId;
+            return this;
+        }
+
+        public Builder setStoreName(String storeName) {
+            this.storeName = storeName;
+            return this;
+        }
+
+        public Builder setStoreAddress(String storeAddress) {
+            this.storeAddress = storeAddress;
+            return this;
+        }
+
+        public Builder setStoreDescription(String storeDescription) {
+            this.storeDescription = storeDescription;
+            return this;
+        }
+
+        public Builder copy(Store store){
+            this.storeId = store.storeId;
+            this.storeName = store.storeName;
+            this.storeAddress = store.storeAddress;
+            this.storeDescription =  store.storeDescription;
+            return this;
+        }
+        public Store build(){
+            return new Store(this);
+        }
+    }
+
+
+}
