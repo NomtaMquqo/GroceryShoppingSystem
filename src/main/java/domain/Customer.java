059ccad3d913246321406c3173560ac99e03bf34
Sinentlahla219081328 <SinentlahlaPindani@gmail.com>
2023-04-04 21:18:00 +0100

    Customer class added

---
+++ src/main/java/domain/Customer.java
@@ -0,0 +1,145 @@
+/**
+ * Customer.java
+ * This is a POJO class for Customer entity
+ * @author Sinentlahla Pindani 219081328
+ * 04 April 2023
+ */
+
+package domain;
+
+public class Customer {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String address;
+
+    private Customer(){
+
+    }
+
+    private Customer(Builder builder) {
+        this.customerId = builder.customerId;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.phone = builder.phone;
+        this.email = builder.email;
+        this.address = builder.address;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastname(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+    public static class Builder{
+        private int customerId;
+        private String firstName;
+        private String lastName;
+        private String phone;
+        private String email;
+        private String address;
+
+
+        public Builder setCustomerId(int customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Customer.Builder copy(Customer customer){
+            this.customerId = customer.customerId;
+            this.firstName = customer.firstName;
+            this.lastName = customer.lastName;
+            this.phone = customer.phone;
+            this.email = customer.email;
+            this.address =  customer.address;
+            return this;
+        }
+
+        public Customer build(){
+
+            return new Customer(this);
+        }
+
+    }
+}
