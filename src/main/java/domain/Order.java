f539ddcf6460a2b9aee8a21a9900db0a41ffaa05
NomtaMquqo <217296580@mycput.ac.za>
2023-04-06 05:36:59 +0200

    add order domain class

---
+++ src/main/java/domain/Order.java
@@ -0,0 +1,103 @@
+package domain;
+
+public class Order {
+    private int id;
+    private String orderNumber;
+    private String type;
+    private String description;
+
+    private Customer customer;
+
+    public Order(Builder builder) {
+        this.id = builder.id;
+        this.orderNumber = builder.orderNumber;
+        this.type = builder.type;
+        this.description = builder.description;
+        this.customer = builder.customer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public static class Builder {
+        private int id;
+        private String orderNumber;
+        private String type;
+        private String description;
+
+        private Customer customer;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+        public Builder setOrderNumber(String orderNumber) {
+            this.orderNumber = orderNumber;
+            return this;
+        }
+
+        public Builder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setCustomer(Customer customer) {
+            this.customer = customer;
+            return this;
+        }
+
+        public Builder copy(String orderNumber, String type, String description, Customer customer) {
+            this.orderNumber = orderNumber;
+            this.type = type;
+            this.description = description;
+            this.customer = customer;
+            return this;
+        }
+
+        public Order build(){
+            return new Order(this);
+        }
+    }
+}
