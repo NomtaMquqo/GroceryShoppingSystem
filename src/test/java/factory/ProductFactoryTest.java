ec066b64ae40210fd1a49efca6c2ee517eac627c
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2023-04-07 21:04:40 +0200

    ProductFactory and ProductFactoryTest classes created. Product class is updated.

---
+++ src/test/java/factory/ProductFactoryTest.java
@@ -0,0 +1,56 @@
+package factory;
+
+import domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductFactoryTest {
+    private Product prod1;
+    private Product prod2;
+    private Product prod3;
+    @BeforeEach
+    void setUp() {
+        prod1 = new Product();
+        prod2 = new Product();
+        prod3 = prod1;
+    }
+    @Test
+    public void test(){
+        Product product = ProductFactory.createProduct( 10, "Meat", "Bread", "Sasko");
+        System.out.println(product.toString());
+        assertNotNull(product);
+    }
+
+    @Test
+    void testEquality(){
+        assertEquals(prod1,prod3);
+    }
+
+    @Test
+    void testIdentity(){
+        assertSame(prod1,prod3);
+    }
+    @Test
+    void testFail(){
+        assertEquals(prod1,prod2);
+    }
+
+    @Test
+    @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
+    void testTimeout() {
+        System.out.println("Timeout has passed");
+    }
+
+    @Test
+    @Disabled("Disabled until bug #42 has been resolved")
+
+    void testWillBeSkipped() {
+    }
+
+}
